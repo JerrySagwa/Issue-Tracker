42c2ccbba0bfd4665fde6dd2ca83b491a711a8ca
sagwa <jerrysagwa0608@gmail.com>
2024-05-05 18:23:52 +0800

    Add Spinner while Submitting

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -6,36 +6,43 @@ import { Controller, useForm, SubmitHandler } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import {createIssueSchema} from '../../validationSchema'
+import { createIssueSchema } from '../../validationSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
-import {z} from 'zod'
-import ErrorMessage from '../../components/ErrorMessage'
+import { z } from 'zod';
+import ErrorMessage from '../../components/ErrorMessage';
+import Spinner from '../../components/Spinner';
 
 type NewIssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
-  const { register, control, handleSubmit, formState: {errors}} = useForm<NewIssueForm>({
-    resolver: zodResolver(createIssueSchema)
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<NewIssueForm>({
+    resolver: zodResolver(createIssueSchema),
   });
   const router = useRouter();
 
   const [error, setError] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const onSubmit: SubmitHandler<NewIssueForm> = async (data) => {
     try {
+      setIsSubmitting(true);
       await axios.post('/api/issues', data);
       router.push('/issues');
     } catch (error) {
       setError('Invalid input of title or description!');
+      setIsSubmitting(false);
     }
   };
   return (
     <div className='max-w-xl'>
       {error && (
         <Callout.Root color='red' className='mb-3'>
-          <Callout.Text>
-            {error}
-          </Callout.Text>
+          <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
       <form className='max-w-xl space-y-4' onSubmit={handleSubmit(onSubmit)}>
@@ -50,8 +57,13 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder='Description' {...field} />
           )}
         />
-        {errors.description && <ErrorMessage>{errors.description?.message}</ErrorMessage>}
-        <Button>Submit New Issue</Button>
+        {errors.description && (
+          <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        )}
+        <Button disabled={isSubmitting}>
+          Submit New Issue
+          {isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
