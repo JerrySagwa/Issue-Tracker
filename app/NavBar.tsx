ff51bbc5a84ba5cc74627d3e595f842ec8432904
sagwa <jerrysagwa0608@gmail.com>
2024-05-08 13:04:37 +0800

    add loading skeleton

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,21 +1,49 @@
 'use client';
-import { BsFillBugFill } from 'react-icons/bs';
-import React from 'react';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import { Button, DropdownMenu, Flex } from '@radix-ui/themes';
 import classnames from 'classnames';
 import { useSession } from 'next-auth/react';
-import { Box, Button, DropdownMenu, Flex } from '@radix-ui/themes';
-import { log } from 'console';
-import { getServerSession } from 'next-auth';
-import { authConfig } from './api/auth/auth';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { BsFillBugFill } from 'react-icons/bs';
 
 type link = {
   href: string;
   label: string;
 };
 
-const NavBar = () => {
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  if (status === 'loading') return <Skeleton width={'3rem'}/>;
+
+  return (
+    <div>
+      {status === 'authenticated' && (
+        <div className='flex gap-x-2 items-center'>
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+              <Button variant='soft'>
+                {session.user?.name}
+                <DropdownMenu.TriggerIcon />
+              </Button>
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content>
+              <DropdownMenu.Item>{session.user?.email}</DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Root>
+          <Link href='/api/auth/signout'>Log Out </Link>
+        </div>
+      )}
+      {status === 'unauthenticated' && (
+        <Link href='/api/auth/signin'>Log In</Link>
+      )}
+    </div>
+  );
+};
+
+const NavLinks = () => {
   const links: link[] = [
     { href: '/', label: 'Dashboard' },
     { href: '/issues', label: 'Issues' },
@@ -23,53 +51,35 @@ const NavBar = () => {
 
   // get path
   const currentPath = usePathname();
-  const { status, data: session } = useSession();
-  // const session = await getServerSession(authConfig);
-  // console.log("session: " + session);
-  console.log('status: ' + status);
-  console.log('session: ' + session);
 
+  return (
+    <>
+      {links.map((l, index) => (
+        <div
+          key={index}
+          className={classnames({
+            'text-zinc-900': currentPath === l.href,
+            'text-zinc-500': currentPath !== l.href,
+            'hover:text-zinc-900 transition-colors': true,
+          })}
+        >
+          <Link href={l.href}>{l.label}</Link>
+        </div>
+      ))}
+    </>
+  );
+};
+
+const NavBar = () => {
   return (
     <div className='flex justify-between border-b mb-5 pl-5 pr-8 h-14 items-center'>
       <Flex justify={'start'} gapX={'4'} align={'center'}>
         <Link href='/'>
           <BsFillBugFill />
         </Link>
-        {links.map((l, index) => (
-          <div
-            key={index}
-            className={classnames({
-              'text-zinc-900': currentPath === l.href,
-              'text-zinc-500': currentPath !== l.href,
-              'hover:text-zinc-900 transition-colors': true,
-            })}
-          >
-            <Link href={l.href}>{l.label}</Link>
-          </div>
-        ))}
+        <NavLinks/>
       </Flex>
-
-      <div className='flex gap-x-2'>
-        {status === 'authenticated' && (
-          <>
-            <DropdownMenu.Root>
-              <DropdownMenu.Trigger>
-                <Button variant='soft'>
-                  {session.user?.name}
-                  <DropdownMenu.TriggerIcon />
-                </Button>
-              </DropdownMenu.Trigger>
-              <DropdownMenu.Content>
-                <DropdownMenu.Item>{session.user?.email}</DropdownMenu.Item>
-              </DropdownMenu.Content>
-            </DropdownMenu.Root>
-            <Link href='/api/auth/signout'>Log Out </Link>
-          </>
-        )}
-        {status === 'unauthenticated' && (
-          <Link href='/api/auth/signin'>Log In</Link>
-        )}
-      </div>
+      <AuthStatus />
     </div>
   );
 };
--- app/issues/components/LoadingFormSkeleton.tsx
+++ app/issues/components/LoadingFormSkeleton.tsx
@@ -1,6 +1,7 @@
 import { Card, Flex } from '@radix-ui/themes';
 import React from 'react';
 import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const LoadingFormSkeleton = () => {
   return (
